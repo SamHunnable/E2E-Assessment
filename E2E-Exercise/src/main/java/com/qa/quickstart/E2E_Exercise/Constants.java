482189040d7c8701691050c5401be8be37a44b60
SamHunnable <sam.hunnable@academytrainee.com>
2018-05-25 12:44:18 +0100

    Changes to constant paths

--- E2E-Exercise/src/main/java/com/qa/quickstart/E2E_Exercise/Constants.java
+++ E2E-Exercise/src/main/java/com/qa/quickstart/E2E_Exercise/Constants.java
@@ -4,16 +4,17 @@ public class Constants {
 	
 	//Universal or Repo based
 	protected static final String DVLAStartPage = "https://www.gov.uk/get-vehicle-information-from-dvla";
-	protected static final String pathToWebDriver = "..\\chromedriver.exe";
-	//protected static final String pathToFile = "..\\TestData.xlsx";
-	//protected static final String pathToReport ="..\\DVLAReport.html";
-	//protected static final String pathToScreenshots = "..\\Screenshots\\";
+	//protected static final String pathToWebDriver = ".\\..\\DriverAndOutputs\\chromedriver.exe";
+	//protected static final String pathToFile = ".\\..\\DriverAndOutputs\\TestData.xlsx";
+	//protected static final String pathToReport =".\\..\\DriverAndOutputs\\DVLAReport.html";
+	//protected static final String pathToScreenshots = ".\\..\\Screenshots\\";
 	
 	//Office path
 	//protected static final String pathToWebDriver = "C:\\Users\\Admin\\WebDrivers\\chromedriver.exe";
-	protected static final String pathToFile = "C:\\Users\\Admin\\E2E\\E2E-Assessment\\TestData.xlsx";
+	protected static final String pathToFile = "C:\\Users\\Admin\\E2E\\E2E-Assessment\\DriverAndOutputs\\TestData.xlsx";
 	protected static final String pathToScreenshots = "C:\\Users\\Admin\\E2E\\E2E-Assessment\\Screenshots\\";
-	protected static final String pathToReport ="C:\\Users\\Admin\\E2E\\E2E-Assessment\\DVLAReport.html";
+	protected static final String pathToReport ="C:\\Users\\Admin\\E2E\\E2E-Assessment\\DriverAndOutputs\\DVLAReport.html";
+	protected static final String pathToWebDriver = "C:\\\\Users\\\\Admin\\\\E2E\\\\E2E-Assessment\\\\DriverAndOutputs\\\\chromedriver.exe";
 	
 	//Home path
 	//protected static final String pathToWebDriver = "C:\\Users\\Sam Hunnable\\QA-Work\\WebDrivers\\chromedriver.exe";
